9a3dee0c9ae50aa21dc5d519c0c325b5a232679b
bnow0806 <guswp1993@naver.com>
2017-07-27 22:26:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/hyunje/calendar/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.hyunje.calendar;
+
+import android.os.Process;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==KeyEvent.KEYCODE_BACK){
+            android.os.Process.killProcess(Process.myPid());
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
